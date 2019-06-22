57373e66351dbb8be6a32272cfbe9a66e722f370
jrquadros <quadrosjunioradits@gmail.com>
2019-06-22 11:39:47 -0300

    setup

---
+++ web/src/services/apolloClient.js
@@ -0,0 +1,48 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+
+const httpLink = new HttpLink({
+  uri: "https://api.graph.cool/simple/v1/cjx7jchzw287u014890onx7on"
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink({
+  uri: `wss://subscriptions.graph.cool/v1/cjx7jchzw287u014890onx7on`,
+  options: {
+    reconnect: true
+  }
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+    }),
+    link
+  ]),
+  cache: new InMemoryCache()
+});
